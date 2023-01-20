bec5d189a7b2faae28c03fbcf46e45c5f2484d47
liamdraper <liamdraper83@gmail.com>
2023-01-19 16:16:27 -0800

    Added wireframe, basic JS

---
+++ js/code.js
@@ -0,0 +1,60 @@
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+let turn, winner, playerBoard, aiBoard;
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+init();
+
+function init() {
+    playerBoard = [
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null]
+    ];
+    aiBoard = [
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null]
+    ];
+    turn = 1;
+    winner = null;
+    render();
+}
+
+function render() {
+    renderBoard();
+    renderMessage();
+    renderControls();
+}
+
+function renderBoard() {
+    playerBoard.forEach(function(colArr, colIdx) {
+        colArr.forEach(function(cell, rowIdx) {
+            const square = document.getElementById(`c${colIdx}r${rowIdx}`);
+            square.style.backgroundColor = 'black';
+        });
+    });
+}
